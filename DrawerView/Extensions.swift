495b69a61a06b030b848fc388763e8b66b6622b9
Vasyl Kreveha <vasyl@simplepractice.com>
2020-04-21 02:27:38 +0300

    selectable drawer orientation

--- DrawerView/Extensions.swift
+++ DrawerView/Extensions.swift
@@ -6,17 +6,19 @@
 //  Copyright © 2018 Mikko Välimäki. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 public extension UIViewController {
 
-    public func addDrawerView(withViewController viewController: UIViewController, parentView: UIView? = nil) -> DrawerView {
+    func addDrawerView(withViewController viewController: UIViewController,
+                              parentView: UIView? = nil,
+                              orientation: DrawerOrientation = .bottom) -> DrawerView {
         #if swift(>=4.2)
         self.addChild(viewController)
         #else
         self.addChildViewController(viewController)
         #endif
-        let drawer = DrawerView(withView: viewController.view)
+        let drawer = DrawerView(withView: viewController.view, orientation: orientation)
         drawer.attachTo(view: self.view)
         return drawer
     }
